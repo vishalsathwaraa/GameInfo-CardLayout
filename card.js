6d2e5fb211907c486ce4e10cd1a926f3eb038d24
vishalsathwaraa <77588402+vishalsathwaraa@users.noreply.github.com>
2021-02-14 00:41:16 +0530

    CssCard Layout
    Game Information With It's Trailer

---
+++ card.js
@@ -0,0 +1,10 @@
+
+      gsap.from(".card1", { y:200,  duration: 1.2, opacity: 0})
+      gsap.from(".card2", { y:-200, duration: 2, opacity: 0, delay: 1 })
+      gsap.from(".card3", { y:200, duration: 2, opacity: 0, delay: 2 })
+      gsap.from(".card4", { y:-200, duration: 2, opacity: 0, delay: 3 })
+      gsap.from(".card5", { x:-200, duration: 2, opacity: 0, delay: 1 })
+      gsap.from(".card6", { x:-200, duration: 2, opacity: 0, delay: 2 })
+      gsap.from(".card7", { x:200, duration: 2, opacity: 0, delay: 3 })
+      gsap.from(".card8", { x:-200, duration: 2, opacity: 0, delay: 4 })
+      gsap.from(".title", { x:-200, duration: 2, opacity: 0, delay: 4 })
